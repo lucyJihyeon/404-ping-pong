ea0ec690b8a977298dff3f197a946e5c6956f1f4
Lucy Kwon <ks02ks222@gmail.com>
2024-02-05 16:32:28 -0800

    chagned the ball position, canvas margin, and velocities

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 // ctx.fillStyle = "#ff0000";
 // //fill out the rectangle with a (x-coord, y-coord, width, height)
 // ctx.fillRect(30, 10, 10, 10);
@@ -9,22 +11,19 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#ff00ff";
 ctx.strokeStyle = "#ff00ff";
 
-let ballx = 50;
-let bally = 40;
+let ballx = 150;
+let bally = 300;
 let ballRadius = 10;
-function gameUpdate() {}
+let ballVelX =2;
+let ballVelY = 2;
 
-function gameDraw() {}
+//function to update the ball position
+function gameUpdate() {
+  ballx += ballVelX;
+  bally += ballVelY;
+}
 
-//function to update the ball's position to implement ball-animation
-function gameLoop() {
-
-  //clear out the canvas 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  window.requestAnimationFrame(gameLoop);
-  ballx += 1;
-  bally += 1;
-
+function gameDraw() {
   //begin drawing the circle
   ctx.beginPath();
   //create a circle positioned at (centerX, centerY, radius, 0 , PI * 2, false: acr will be drawn in a clockwide direction )
@@ -33,7 +32,14 @@ function gameLoop() {
   ctx.fill();
   //draw the outline.
   ctx.stroke();
+}
 
+//function to update the ball's position to implement the animation loop
+function gameLoop() {
+  //clear out the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //gameloop is called 60times/s
+  window.requestAnimationFrame(gameLoop);
 
   gameUpdate();
   gameDraw();
