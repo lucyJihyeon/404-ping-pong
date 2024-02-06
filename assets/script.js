9f46be4e2c06f875ee74f81611dddc0c515fa4e7
Lucy Kwon <ks02ks222@gmail.com>
2024-02-05 16:43:52 -0800

    Created a Ball class that creates a circle

--- assets/script.js
+++ assets/script.js
@@ -7,31 +7,45 @@ canvas.height = window.innerHeight;
 // ctx.fillStyle = "#ff0000";
 // //fill out the rectangle with a (x-coord, y-coord, width, height)
 // ctx.fillRect(30, 10, 10, 10);
-
 ctx.fillStyle = "#ff00ff";
 ctx.strokeStyle = "#ff00ff";
 
-let ballx = 150;
-let bally = 300;
-let ballRadius = 10;
-let ballVelX =2;
-let ballVelY = 2;
+//converting the x coordinate and y coordinate as a properties in an object
+function vec2(x, y) {
+  return { x: x, y: y };
+}
+
+function Ball(pos, velocity, radius) {
+  this.pos = pos;
+  this.velocity = velocity;
+  this.radius = radius;
+
+  this.update = function () {
+    this.pos.x += this.velocity.x;
+    this.pos.y += this.velocity.y;
+  };
+
+  this.draw = function () {
+    //begin drawing the circle
+    ctx.beginPath();
+    //create a circle positioned at (centerX, centerY, radius, 0 , PI * 2, false: acr will be drawn in a clockwide direction )
+    ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2, false);
+    //fill out the circle
+    ctx.fill();
+    //draw the outline.
+    ctx.stroke();
+  };
+}
+
+const ball = new Ball(vec2(150, 300), vec2(2.5, 1), 10);
 
 //function to update the ball position
 function gameUpdate() {
-  ballx += ballVelX;
-  bally += ballVelY;
+  ball.update();
 }
 
 function gameDraw() {
-  //begin drawing the circle
-  ctx.beginPath();
-  //create a circle positioned at (centerX, centerY, radius, 0 , PI * 2, false: acr will be drawn in a clockwide direction )
-  ctx.arc(ballx, bally, ballRadius, 0, Math.PI * 2, false);
-  //fill out the circle
-  ctx.fill();
-  //draw the outline.
-  ctx.stroke();
+  ball.draw();
 }
 
 //function to update the ball's position to implement the animation loop
