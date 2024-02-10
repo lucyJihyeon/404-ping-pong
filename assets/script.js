f5008dc6596c445e9a36987e997d755dae9b2390
Lucy Kwon <ks02ks222@gmail.com>
2024-02-09 20:40:29 -0800

    built a simple AI function to move the paddle played by a bot

--- assets/script.js
+++ assets/script.js
@@ -15,11 +15,11 @@ const KeyUp = 38;
 const KeyDown = 40;
 
 //when a key is pressed, the keycode of the pressed key is stored in the 'keyPressed' array with a value of true
-window.addEventListener('keydown', function(e) {
+window.addEventListener("keydown", function (e) {
   keyPressed[e.keyCode] = true;
 });
 //when a key is released, the keycode of the corresponding key is stored in the 'keyPressed' array with a value of false
-window.addEventListener('keyup', function(e) {
+window.addEventListener("keyup", function (e) {
   keyPressed[e.keyCode] = false;
 });
 
@@ -28,64 +28,64 @@ function vec2(x, y) {
   return { x: x, y: y };
 }
 
-function ballCollosion(ball)    {
-    //if the y coordinate of the bottom of the ball (the centerY + radius) touches the bottom side of the canvas, the ball moves upward
-    if (ball.pos.y + ball.radius >= canvas.height)  {
-        //velocity has to be negative value because it is going upward
-        ball.velocity.y *= -1;
-    }
-    //if the ball touches the top, bounces bounces back 
-    if (ball.pos.y + ball.radius <= marginTop)  {
-        ball.velocity.y *= -1;
-    }
-    //if the x coordinate of the bottom of the ball (the centerX + radius) touches the right wall, the ball bounces to the other direction
-    if (ball.pos.x + ball.radius >= canvas.width) {
-        ball.velocity.x *= -1;
-    }
-    //if the ball touches the left wall, the ball bounces to the orhter direction.
-    if (ball.pos.x + ball.radius <= 0) {
-        ball.velocity.x *= -1;
-    }
+function ballCollosion(ball) {
+  //if the y coordinate of the bottom of the ball (the centerY + radius) touches the bottom side of the canvas, the ball moves upward
+  if (ball.pos.y + ball.radius >= canvas.height) {
+    //velocity has to be negative value because it is going upward
+    ball.velocity.y *= -1;
+  }
+  //if the ball touches the top, bounces bounces back
+  if (ball.pos.y + ball.radius <= marginTop) {
+    ball.velocity.y *= -1;
+  }
+  //if the x coordinate of the bottom of the ball (the centerX + radius) touches the right wall, the ball bounces to the other direction
+  if (ball.pos.x + ball.radius >= canvas.width) {
+    ball.velocity.x *= -1;
+  }
+  //if the ball touches the left wall, the ball bounces to the orhter direction.
+  if (ball.pos.x + ball.radius <= 0) {
+    ball.velocity.x *= -1;
+  }
 }
 
 //function to handle the paddle movement range
-function paddleCollision(paddle)  {
+function paddleCollision(paddle) {
   //if the paddle touches the top, stay.
-  if (paddle.pos.y <= marginTop)  {
+  if (paddle.pos.y <= marginTop) {
     paddle.pos.y = marginTop;
   }
   //if the paddle touches the bottom, stay.
-  if (paddle.pos.y + paddle.height >= canvas.height)  {
+  if (paddle.pos.y + paddle.height >= canvas.height) {
     paddle.pos.y = canvas.height - paddle.height;
   }
 }
 
 //function to create a ping-pong paddle
-function Paddle(pos,velocity, width, height) {
+function Paddle(pos, velocity, width, height) {
   this.pos = pos;
   this.velocity = velocity;
   this.width = width;
   this.height = height;
 
   //method to move the ping pong paddle up and down
-  this.update = function() {
+  this.update = function () {
     //when key up, move upward
-    if(keyPressed[KeyUp]){
+    if (keyPressed[KeyUp]) {
       this.pos.y -= this.velocity.y;
     }
     //when key down, move down
-    if(keyPressed[KeyDown]){
+    if (keyPressed[KeyDown]) {
       this.pos.y += this.velocity.y;
     }
   };
 
   //draw method to create a rectangle(paddle) that takes x-center, y-center, width, and height)
-  this.draw = function() {
+  this.draw = function () {
     ctx.fillStyle = "#33ff00";
     ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height);
   };
   //method to get half width of the paddle
-  this.getHalfWidth = function() {
+  this.getHalfWidth = function () {
     return this.width / 2;
   };
   //method to get half height of the paddle
@@ -93,11 +93,13 @@ function Paddle(pos,velocity, width, height) {
     return this.height / 2;
   };
   //method to get the center of the paddle
-  this.getCenter = function() {
-    return vec2(this.pos.x + this.getHalfWidth(), this.pos.y + this.getHalfHeight());
-  }
+  this.getCenter = function () {
+    return vec2(
+      this.pos.x + this.getHalfWidth(),
+      this.pos.y + this.getHalfHeight()
+    );
+  };
 }
-
 
 //Ball class to create a ball that takes x and y coordinate position, velocity, and radius
 function Ball(pos, velocity, radius) {
@@ -111,7 +113,7 @@ function Ball(pos, velocity, radius) {
     this.pos.y += this.velocity.y;
   };
 
-  //draw method to draw a ball 
+  //draw method to draw a ball
   this.draw = function () {
     //begin drawing the circle
     ctx.beginPath();
@@ -122,15 +124,17 @@ function Ball(pos, velocity, radius) {
     //draw the outline.
     ctx.stroke();
   };
-  
-};
+}
 
 //function to implement a ball bouncing when it touches the paddle
 function ballPaddleCollision(ball, paddle) {
   let dx = Math.abs(ball.pos.x - paddle.getCenter().x);
   let dy = Math.abs(ball.pos.y - paddle.getCenter().y);
 
-  if (dx <= (ball.radius + paddle.getHalfWidth()) && dy <= (paddle.getHalfHeight() + ball.radius))  {
+  if (
+    dx <= ball.radius + paddle.getHalfWidth() &&
+    dy <= paddle.getHalfHeight() + ball.radius
+  ) {
     ball.velocity.x *= -1;
   }
 }
@@ -138,26 +142,56 @@ function ballPaddleCollision(ball, paddle) {
 //create a ball
 const ball = new Ball(vec2(150, 300), vec2(2, 2), 10);
 //create paddles on each sides
-const paddleLeft = new Paddle(vec2(0,50), vec2(5,5), 20, 100);
+const paddleLeft = new Paddle(vec2(0, 50), vec2(5, 5), 20, 100);
 //x-center should be width - 20 because it occupies 20 width of the canvas
-const paddleRight = new Paddle(vec2(canvas.width-20, 50), vec2(5,5), 20, 100);
+const paddleRight = new Paddle(
+  vec2(canvas.width - 20, 50),
+  vec2(5, 5),
+  20,
+  100
+);
 
-
-//function to update the ball and paddles position 
+//function to update the ball and paddles position
 function gameUpdate() {
   ball.update();
   paddleLeft.update();
-  paddleCollision(paddleLeft)
+  paddleCollision(paddleLeft);
   //paddleRight.update();
   ballCollosion(ball);
-  ballPaddleCollision(ball,paddleLeft);
-};
+  ballPaddleCollision(ball, paddleLeft);
+  player2AI(ball,paddleRight);
+}
 
 //dynamically update the ball and paddle's position by updating its position
 function gameDraw() {
   ball.draw();
   paddleLeft.draw();
   paddleRight.draw();
+}
+
+//functino to create a player 2 (AI)
+function player2AI(ball, paddle) {
+  //if the ball is moving to the rightside
+  if (ball.velocity.x > 0) {
+    //and the ball is below the paddle
+    if (ball.pos.y > paddle.pos.y) {
+      //move the paddle downside
+      paddle.pos.y += paddle.velocity.y;
+      //make sure the paddle doesn't pass the canvas 
+      if (paddle.pos.y + paddle.height >= canvas.height) {
+        paddle.pos.y = canvas.height - paddle.height;
+      }
+    }
+    //if the ball is above the paddle
+    if (ball.pos.y < paddle.pos.y) {
+      //move the paddle upward
+      paddle.pos.y -= paddle.velocity.y;
+      //make sure the paddle doesn't go above the canvas
+      if (paddle.pos.y <= marginTop) {
+        paddleLeft.pos.y = marginTop;
+      }
+    }
+  }
 }
 
 //function to update the ball's position to implement the animation loop
